1f142e7f02b55f88d0f89fa04ef178d4e76010cc
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2022-04-12 13:33:40 +1200

    Added panel component to the board.

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,62 +1,76 @@
 import React from "react"
 import "./Board.css"
+import BottomPanel from "../Panel/BottomPanel"
+import TopPanel from "../Panel/TopPanel"
 
 const Board = () => {
 
     return (
-        <div class="boardcontainer">
-            <div id="board">
-                <table>
-                    <tr id="row1">
-                        <td class="square h"></td>
-                        <td class="square v h"></td>
-                        <td class="square h"></td>
-                        <td class="square v h"></td>
-                        <td class="square h"></td>
-                        <td class="square v h"></td>
-                    </tr>
-                    <tr id="row2">
-                        <td class="square h"></td>
-                        <td class="square v h"></td>
-                        <td class="square h"></td>
-                        <td class="square v h"></td>
-                        <td class="square h"></td>
-                        <td class="square v h"></td>
-                    </tr>
-                    <tr id="row3">
-                        <td class="square h"></td>
-                        <td class="square v h"></td>
-                        <td class="square h"></td>
-                        <td class="square v h"></td>
-                        <td class="square h"></td>
-                        <td class="square v h"></td>
-                    </tr>
-                    <tr id="row4">
-                        <td class="square h"></td>
-                        <td class="square v h"></td>
-                        <td class="square h"></td>
-                        <td class="square v h"></td>
-                        <td class="square h"></td>
-                        <td class="square v h"></td>
-                    </tr>
-                    <tr id="row5">
-                        <td class="square h"></td>
-                        <td class="square v h"></td>
-                        <td class="square h"></td>
-                        <td class="square v h"></td>
-                        <td class="square h"></td>
-                        <td class="square v h"></td>
-                    </tr>
-                    <tr id="row6">
-                        <td class="square h"></td>
-                        <td class="square v h"></td>
-                        <td class="square h"></td>
-                        <td class="square v h"></td>
-                        <td class="square h"></td>
-                        <td class="square v h"></td>
-                    </tr>
-                </table>
+        <div class="container">
+            <div class="boardcontainer">
+                <div id="board">
+                    <table>
+                        <tr id="row1">
+                            <td class="square h"></td>
+                            <td class="square v h"></td>
+                            <td class="square h"></td>
+                            <td class="square v h"></td>
+                            <td class="square h"></td>
+                            <td class="square v h"></td>
+                        </tr>
+                        <tr id="row2">
+                            <td class="square h"></td>
+                            <td class="square v h"></td>
+                            <td class="square h"></td>
+                            <td class="square v h"></td>
+                            <td class="square h"></td>
+                            <td class="square v h"></td>
+                        </tr>
+                        <tr id="row3">
+                            <td class="square h"></td>
+                            <td class="square v h"></td>
+                            <td class="square h"></td>
+                            <td class="square v h"></td>
+                            <td class="square h"></td>
+                            <td class="square v h"></td>
+                        </tr>
+                        <tr id="row4">
+                            <td class="square h"></td>
+                            <td class="square v h"></td>
+                            <td class="square h"></td>
+                            <td class="square v h"></td>
+                            <td class="square h"></td>
+                            <td class="square v h"></td>
+                        </tr>
+                        <tr id="row5">
+                            <td class="square h"></td>
+                            <td class="square v h"></td>
+                            <td class="square h"></td>
+                            <td class="square v h"></td>
+                            <td class="square h"></td>
+                            <td class="square v h"></td>
+                        </tr>
+                        <tr id="row6">
+                            <td class="square h"></td>
+                            <td class="square v h"></td>
+                            <td class="square h"></td>
+                            <td class="square v h"></td>
+                            <td class="square h"></td>
+                            <td class="square v h"></td>
+                        </tr>
+                    </table>
+
+                </div>
+
+
             </div>
+            <div class="top-panel">
+                <TopPanel />
+                <div>
+                    <BottomPanel />
+                </div>
+            </div>
+
         </div>
     )
 }
