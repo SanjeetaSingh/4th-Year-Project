b0ab62c1490d05434f95ec9c87122bbf1de774ea
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2022-04-12 13:32:51 +1200

    Added 2 panels for the code and buttons.

---
+++ src/components/Panel/BottomPanel.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./BottomPanel.css"
+
+const BottomPanel = () => {
+    return (
+        <div class="border">
+            <p>Instructions and buttons</p>
+            <p>will go here</p>
+        </div>
+    );
+}
+
+export default BottomPanel;
---
+++ src/components/Panel/TopPanel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./TopPanel.css"
+
+const TopPanel = () => {
+    return (
+        <div class="borders">
+            <p>code will show here</p>
+        </div>
+    );
+}
+
+export default TopPanel;
