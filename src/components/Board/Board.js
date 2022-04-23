5f695c2a44105ffc99d455c0f1c8224ed7c1dcd8
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2022-04-23 23:38:06 +1200

    Added image for tiles for board and made the boaard iterative.

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -2,66 +2,25 @@ import React from "react"
 import "./Board.css"
 import BottomPanel from "../Panel/BottomPanel"
 import TopPanel from "../Panel/TopPanel"
+import tile from '../../assets/tile.jpg'
 
 const Board = () => {
+
+    const col = ["1", "2", "3", "4", "5"]
+    const row = ["1", "2", "3", "4", "5"]
+
+    let board = [];
+
+    for (let i = col.length - 1; i >= 0; i--) {
+        for (let j = 0; j < row.length; j++) {
+            board.push(<div class="tiles">{<img src={tile} alt="tile" />}</div>)
+        }
+    }
 
     return (
         <div class="container">
             <div class="boardcontainer">
-                <div id="board">
-                    <table>
-                        <tr id="row1">
-                            <td class="square h"></td>
-                            <td class="square v h"></td>
-                            <td class="square h"></td>
-                            <td class="square v h"></td>
-                            <td class="square h"></td>
-                            <td class="square v h"></td>
-                        </tr>
-                        <tr id="row2">
-                            <td class="square h"></td>
-                            <td class="square v h"></td>
-                            <td class="square h"></td>
-                            <td class="square v h"></td>
-                            <td class="square h"></td>
-                            <td class="square v h"></td>
-                        </tr>
-                        <tr id="row3">
-                            <td class="square h"></td>
-                            <td class="square v h"></td>
-                            <td class="square h"></td>
-                            <td class="square v h"></td>
-                            <td class="square h"></td>
-                            <td class="square v h"></td>
-                        </tr>
-                        <tr id="row4">
-                            <td class="square h"></td>
-                            <td class="square v h"></td>
-                            <td class="square h"></td>
-                            <td class="square v h"></td>
-                            <td class="square h"></td>
-                            <td class="square v h"></td>
-                        </tr>
-                        <tr id="row5">
-                            <td class="square h"></td>
-                            <td class="square v h"></td>
-                            <td class="square h"></td>
-                            <td class="square v h"></td>
-                            <td class="square h"></td>
-                            <td class="square v h"></td>
-                        </tr>
-                        <tr id="row6">
-                            <td class="square h"></td>
-                            <td class="square v h"></td>
-                            <td class="square h"></td>
-                            <td class="square v h"></td>
-                            <td class="square h"></td>
-                            <td class="square v h"></td>
-                        </tr>
-                    </table>
-
-                </div>
-
+                <div class="board"> {board}</div>
 
             </div>
             <div class="top-panel">
