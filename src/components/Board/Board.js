1d76850e667714ad803316acfd3dfd40da2c2863
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2022-04-24 21:47:32 +1200

    Showing the dog image on the board.

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -2,28 +2,44 @@ import React from "react"
 import "./Board.css"
 import BottomPanel from "../Panel/BottomPanel"
 import TopPanel from "../Panel/TopPanel"
-import tile from '../../assets/tile.jpg'
+import Tile from "../Tile/Tiles"
+import Objects from "../objects"
+// import dog from './assets/dog.png'
+const col = ["1", "2", "3", "4", "5"];
+const row = ["1", "2", "3", "4", "5"];
+
+const p = [Objects];
+
+    p.push({ image: "assets/dog.png", x: 2, y: 2 });
+
 
 const Board = () => {
-
-    const col = ["1", "2", "3", "4", "5"]
-    const row = ["1", "2", "3", "4", "5"]
 
     let board = [];
 
     for (let i = col.length - 1; i >= 0; i--) {
         for (let j = 0; j < row.length; j++) {
-            board.push(<div class="tiles">{<img src={tile} alt="tile" />}</div>)
+            const num = j + i + 2
+            let image = undefined;
+
+            p.forEach((el) => {
+                if (el.x === j && el.y === i) {
+                    image = el.image;
+                }
+            });
+
+            console.log(image)
+            board.push(<Tile image={image} num={num} />);
         }
     }
 
     return (
-        <div class="container">
-            <div class="boardcontainer">
-                <div class="board"> {board}</div>
+        <div className="container">
+            <div className="boardcontainer">
+                <div className="board"> {board}</div>
 
             </div>
-            <div class="top-panel">
+            <div className="top-panel">
                 <TopPanel />
                 <div>
                     <BottomPanel />
