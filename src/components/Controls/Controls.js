c4e900697aaa28290f491e496a784a2f0c168357
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2022-06-05 15:04:43 +1200

    Added css file for controls and when button is clicked string for move prints on console.

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -1,6 +1,12 @@
 import React from 'react'
-
 const Controls = () => {
+
+    //String that will show when button is clicked
+    let value = ""
+
+    //List of strings
+    let list = []
+
 
     function moveLeft() {
         var element = document.getElementById('dog');
@@ -9,6 +15,14 @@ const Controls = () => {
         const col = parseInt(box.getAttribute('data-col')) - 1;
         const newBox = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
         newBox.append(element);
+        value = "moveLeft()"
+        list.push(value)
+
+        //Getting the last element of the list
+        const lastVal = Object.keys(list).pop()
+        const item = list[lastVal]
+
+        console.log(item)
     }
 
     function moveRight() {
@@ -18,6 +32,17 @@ const Controls = () => {
         const col = parseInt(box.getAttribute('data-col')) + 1;
         const newBox = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
         newBox.append(element);
+
+        value = "moveRight()"
+
+        list.push(value)
+
+        //Getting the last element of the list
+        const lastVal = Object.keys(list).pop()
+        const item = list[lastVal]
+
+        console.log(item)
+
     }
 
     function moveUp() {
@@ -27,6 +52,16 @@ const Controls = () => {
         const col = parseInt(box.getAttribute('data-col'));
         const newBox = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
         newBox.append(element);
+
+        value = "moveUp()"
+        list.push(value)
+
+        //Getting the last element of the list
+        const lastVal = Object.keys(list).pop()
+        const item = list[lastVal]
+
+        console.log(item)
+
     }
 
     function moveDown() {
@@ -36,15 +71,26 @@ const Controls = () => {
         const col = parseInt(box.getAttribute('data-col'));
         const newBox = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
         newBox.append(element);
+
+        value = "moveDown()"
+        list.push(value)
+
+        //Getting the last element of the list
+        const lastVal = Object.keys(list).pop()
+        const item = list[lastVal]
+
+        console.log(item)
+
     }
-    
+
+
     return (
         <div>
             <button class="button" onClick={moveLeft}>Left</button>
             <button class="button" onClick={moveRight}>Right</button>
             <button class="button" onClick={moveUp}>Up</button>
             <button class="button" onClick={moveDown}>Down</button>
-            <button class="submit">Submit</button>
+            <button class="submit" id="sub">Submit</button>
         </div>
     );
 }
