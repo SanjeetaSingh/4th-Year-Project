7f0dafc29b37afad882de3e8f2a47ca0ba7ecd89
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2022-06-10 23:27:41 +1200

    Made the board bigger. 7x7

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -3,22 +3,22 @@ import "./Board.css"
 import BottomPanel from "../Panel/BottomPanel"
 
 const state = {
-    board: Array(25).fill(null),
+    board: Array(49).fill(null),
     player: null
 }
 
-const Board = () => {
+const Board = () => {                                                                                                                                                                                                                                                                                                   
 
     const renderBoxes = () => {
         return state.board.map(
             (box, index) =>
-                <div className="box" data-row={parseInt(index / 5)} data-col={index % 5}>
+                <div className="box" data-row={parseInt(index / 7)} data-col={index % 7}>
                     {box}
                 </div>
         )
     }
 
-    state.board[12] = <img class="image" src="assets/dog.png" alt="d" id="dog"></img>
+    state.board[24] = <img class="image" src="assets/dog.png" alt="d" id="dog"></img>
 
     state.board[0] = <img class="bone" src="assets/food.png" alt="f" id="food"></img>
 
