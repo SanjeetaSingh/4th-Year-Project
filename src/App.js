59885f4994bc56ebe1b23e1d992e6f6881a90fbc
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2022-06-29 14:37:40 +1200

    Added a route to another board for level 2.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Board2 from './components/Level 2/L2-Board/Level-2-Board';
 import Header from './components/Header/Header';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Switch>
           <Route exact path='/'> <Home /></Route>
           <Route path='/about'><About /></Route>
+          <Route path='/level2'><Board2 /></Route>
         </Switch>
       </div>
     </Router>
