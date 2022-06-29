fe0fc21c5e442739948ecd273369034181e604ab
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2022-06-29 14:40:17 +1200

    Added level 2 files and created a level 2 board.

---
+++ src/components/Level 2/L2-Board/Level-2-Board.js
@@ -0,0 +1,52 @@
+import React from "react"
+import "./Level-2-Board.css"
+import Panel from "../L2-Panel/Level-2-Panel"
+
+const state = {
+    board: Array(49).fill(null),
+    player: null
+}
+
+const Board2 = () => {                                                                                                                                                                                                                                                                                                   
+
+    const renderBoxes = () => {
+        return state.board.map(
+            (box, index) =>
+                <div className="box" data-row={parseInt(index / 7)} data-col={index % 7}>
+                    {box}
+                </div>
+        )
+    }
+
+    state.board[40] = <img class="dog" src="assets/dog.png" alt="d" id="dog"></img>
+
+    state.board[0] = <img class="food" src="assets/food.png" alt="f" id="food"></img>
+
+    state.board[7] = <img class="hole" src="assets/hole.png" alt="h" id="hole"></img>
+
+    state.board[38] = <img class="hole" src="assets/hole.png" alt="h" id="hole"></img>
+
+    state.board[13] = <img class="food" src="assets/food.png" alt="f" id="food"></img>
+
+    state.board[17] = <img class="cat" src="assets/cat.png" alt="c" id="cat"></img>
+
+
+
+    return (
+        <div className="container" >
+            <div className="boardcontainer">
+                <div className="board" >
+                    {renderBoxes()}
+                </div>
+            </div>
+
+
+            <div className="bottom-panel">
+                <Panel/>
+            </div>
+
+        </div>
+    )
+}
+
+export default Board2;
---
+++ src/components/Level 2/L2-Control/Level-2-Control.js
@@ -0,0 +1,366 @@
+import React from 'react'
+import Popup from 'react-popup';
+import "./Level-2-Control.css"
+const Controls = () => {
+
+    /**
+     * Variables
+     */
+
+    //String that will show when button is clicked
+    let value = ""
+
+    //List of strings
+    let list = []
+
+    // Stores all the moves to be done on submit
+    let moves = []
+
+    // Checking if the submit button is pressed
+    let pressed = false
+
+    // Counting how many times a button has been selected
+    let count = 0
+    // Adding the count to an array
+    let commands = []
+
+    // To sum up the values of the commands array
+    let total = 0
+
+
+    /**
+     * Submits the sequence that the user
+     * has entered for the dog object to move.
+     * The dog object will move after submit is pressed
+     */
+    function submit() {
+
+        let items = moves.values();
+
+        pressed = true
+
+        // Iterating through all the moves in the array to know which move to do
+        for (let element of items) {
+            if (element === "left") {
+                moveLeft()
+            }
+            if (element === "right") {
+                moveRight()
+            }
+            if (element === "down") {
+                moveDown()
+            }
+            if (element === "up") {
+                moveUp()
+            }
+        }
+    }
+
+    /**
+     * Moves the dog object one tile to the left
+     */
+    function moveLeft() {
+        var element = document.getElementById('dog');
+        const box = element.parentElement;
+        const row = parseInt(box.getAttribute('data-row'))
+        const col = parseInt(box.getAttribute('data-col')) - 1;
+        const newBox = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
+        newBox.append(element);
+
+        statement()
+    }
+
+    /**
+     * Move the dog object one tile to the right
+     */
+    function moveRight() {
+        var element = document.getElementById('dog');
+        const box = element.parentElement;
+        const row = parseInt(box.getAttribute('data-row'))
+        const col = parseInt(box.getAttribute('data-col')) + 1;
+        const newBox = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
+        newBox.append(element);
+
+        statement()
+
+    }
+
+
+    /**
+     * Moves the dog object one tile up
+     */
+    function moveUp() {
+        var element = document.getElementById('dog');
+        const box = element.parentElement;
+        const row = parseInt(box.getAttribute('data-row')) - 1
+        const col = parseInt(box.getAttribute('data-col'));
+        const newBox = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
+        newBox.append(element);
+
+        statement()
+    }
+
+    /**
+     * Moves the dog object one tile down
+     */
+    function moveDown() {
+        var element = document.getElementById('dog');
+        const box = element.parentElement;
+        const row = parseInt(box.getAttribute('data-row')) + 1
+        const col = parseInt(box.getAttribute('data-col'));
+        const newBox = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
+        newBox.append(element);
+
+        statement()
+    }
+
+
+    /**
+     * Method checks if the dog object is at the 
+     * end postion and if the user has completed
+     * the level successfully
+     */
+    function statement() {
+
+        var dog = document.getElementById('dog');
+        var food = document.getElementById('food');
+
+        if (dog != null || food != null) {
+            const box = dog.parentElement;
+            let foodbox = food.parentElement;
+
+            const row = parseInt(box.getAttribute('data-row'))
+            const col = parseInt(box.getAttribute('data-col'))
+
+            const foodrow = parseInt(foodbox.getAttribute('data-row'))
+            const foodcol = parseInt(foodbox.getAttribute('data-col'))
+
+            if (row === foodrow && col === foodcol) {
+                document.getElementById('food').src = "assets/dog.png"
+                if (count <= 10) {
+                    <div>
+                        {Popup.clearQueue()}
+                        {Popup.create({
+                            title: 'Success',
+                            content: 'You completed the level, Good Work! ',
+                            buttons: {
+                                right: [{
+                                    text: 'Okay',
+                                    className: 'success',
+                                    action: function () {
+                                        window.location.reload(true)
+                                        //instead of reloading the page i will have to move to next level
+                                        Popup.clearQueue();
+                                        Popup.close()
+                                    }
+                                }]
+                            }
+                        }, true)}
+                    </div>
+
+                }
+            } else {
+                <div>
+                    {Popup.clearQueue()}
+                    {Popup.create({
+                        title: 'Failed',
+                        content: 'You failed to complete the level, Try Again!',
+                        buttons: {
+                            right: [{
+                                text: 'Try Again',
+                                className: 'danger',
+                                action: function () {
+                                    window.location.reload(true)
+                                    Popup.clearQueue();
+                                    Popup.close()
+                                }
+                            }]
+                        }
+                    }, true)}
+
+                </div>
+            }
+        }
+    }
+
+
+    /**
+     * Adds a string to an array to that
+     * represents the movement left. This
+     * will be compared in the submit method
+     * that will move the dog object at the end
+     */
+    function addLeft() {
+        let left = "left"
+
+        moves.push(left)
+
+        value = "moveLeft();"
+        list.push(value)
+
+        //Getting the last element of the list
+        const lastVal = Object.keys(list).pop()
+        const item = list[lastVal]
+        if (pressed !== true) {
+            if (list.length <= 10) {
+                document.getElementById("action").innerHTML += item + "<br/>"
+            }
+            count += 1
+            commands.push(count)
+
+            for (const element of commands) {
+                total = element
+                document.getElementById("count").innerHTML = total + "/10"
+            }
+        }
+    }
+
+
+    /**
+     * Adds a string to an array to that
+     * represents the movement right. This
+     * will be compared in the submit method
+     * that will move the dog object at the end
+     */
+    function addRight() {
+        let right = "right"
+
+        moves.push(right)
+
+        value = "moveRight();"
+        list.push(value)
+
+        //Getting the last element of the list
+        const lastVal = Object.keys(list).pop()
+        const item = list[lastVal]
+
+        if (pressed !== true) {
+            if (list.length <= 10) {
+                document.getElementById("action").innerHTML += item + "<br/>"
+            }
+
+            count += 1
+            commands.push(count)
+
+            for (const element of commands) {
+                total = element
+                document.getElementById("count").innerHTML = total + "/10"
+            }
+        }
+    }
+
+
+    /**
+     * Adds a string to an array to that
+     * represents the movement down. This
+     * will be compared in the submit method
+     * that will move the dog object at the end
+     */
+    function addDown() {
+        let down = "down"
+
+        moves.push(down)
+        value = "moveDown();"
+        list.push(value)
+
+        //Getting the last element of the list
+        const lastVal = Object.keys(list).pop()
+        const item = list[lastVal]
+
+        if (pressed !== true) {
+            if (list.length <= 10) {
+                document.getElementById("action").innerHTML += item + "<br/>"
+            }
+
+            count += 1
+            commands.push(count)
+
+            for (const element of commands) {
+                total = element
+                document.getElementById("count").innerHTML = total + "/10"
+            }
+        }
+    }
+
+
+    /**
+     * Adds a string to an array to that
+     * represents the movement up. This
+     * will be compared in the submit method
+     * that will move the dog object at the end
+     */
+    function addUp() {
+        let up = "up"
+
+        moves.push(up)
+
+        value = "moveUp();"
+        list.push(value)
+
+        //Getting the last element of the list
+        const lastVal = Object.keys(list).pop()
+        const item = list[lastVal]
+
+        if (pressed !== true) {
+            if (list.length <= 10) {
+                document.getElementById("action").innerHTML += item + "<br/>"
+            }
+            count += 1
+            commands.push(count)
+
+            for (const element of commands) {
+                total = element
+                document.getElementById("count").innerHTML = total + "/10"
+            }
+        }
+    }
+
+    /**
+     * Added a clear button to remove the 
+     * sequence from the panel. And reset the 
+     * game.
+     */
+    function clearAll() {
+        list = []
+        moves = []
+        commands = []
+        count = 0
+        document.getElementById("action").innerHTML = ""
+        document.getElementById("count").innerHTML = "/10"
+    }
+
+    return (
+        <div class="containers">
+            <div class="borderPanel">
+                <h2>Level 2:</h2>
+                <div class="toppart">
+                    <div class="containing">
+                        <p id="count" class="number">0/10</p>
+                    </div>
+                    <p class="titles">Sequence:</p>
+                    <p id="action" class="lists"></p>
+
+                </div>
+
+
+                <div class="bubble bubble-bottom-left" >
+                    Hi there! Your aim for this level is to get the dog to the food with the command butttons below. You are only allowed to use 10 commands for your
+                    sequence, so try to find the shortest path to get to the food. Once you are done with creating your sequence then go ahead and click the submit button!
+                </div>
+            </div>
+
+            <div class="outside">
+                <button type='submit' class="button" onClick={addLeft} disabled={pressed === true} >Left</button>
+                <button type='submit' class="button" onClick={addUp} disabled={pressed === true} >Up</button>
+                <button type='submit' class="button" onClick={addDown} disabled={pressed === true} >Down</button>
+                <button type='submit' class="button" onClick={addRight} disabled={pressed === true} >Right</button>
+                <button type='submit' class="button" onClick={submit} disabled={pressed === true} > Submit</button>
+                <button type='submit' class="button" onClick={clearAll} disabled={pressed === true}>Clear</button>
+            </div>
+
+
+        </div>
+    );
+}
+
+export default Controls;
