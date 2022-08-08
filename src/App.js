c3c348a3e9269aa3dc263e00bdd91c957af3aa8f
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2022-08-08 13:21:20 +1200

    Made some alterations to the other files.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 import Home from './pages/Home';
 import About from './pages/About';
 import Board2 from './components/Level-2/L2-Board/Level-2-Board';
+import Board3 from './Level3/Board/Board-3'
 import Header from './components/Header/Header';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           <Route exact path='/'> <Home /></Route>
           <Route path='/about'><About /></Route>
           <Route path='/level2'><Board2 /></Route>
+          <Route path='/level3'><Board3 /></Route>
         </Switch>
       </div>
     </Router>
