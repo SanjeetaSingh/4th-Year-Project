62e87bed924390609dddfa888e78b7c2b94576aa
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2022-08-26 18:53:35 +1200

    Implemented template files for level 6.

---
+++ src/Level6/L6-Panel/L6-Panel.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../../Level2/L2-Panel/Level-2-Panel.css"
+import Controls from '../L6-Control/L6-Control';
+
+const Panel6 = () => {
+    return (
+        <div class="border">
+            <Controls/>
+        </div>
+    );
+}
+
+export default Panel6;
