81de3f4ee23efc0993036cfecb23591f97e6963f
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2022-09-17 13:28:05 +1200

    Made template for level 10 and made 2 cats moving.

---
+++ src/Level10/L10-Panel/L10-Panel.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../../Style/Panel.css"
+import Controls from '../L10-Control/L10-Control';
+
+const Panel10 = () => {
+    return (
+        <div class="border">
+            <Controls/>
+        </div>
+    );
+}
+
+export default Panel10;
