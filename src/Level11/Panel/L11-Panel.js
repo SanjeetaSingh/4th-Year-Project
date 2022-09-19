fe33ea5d1ee13164b0e617349b2e19fdac694ad8
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2022-09-19 19:58:11 +1200

    Created level 11.

---
+++ src/Level11/Panel/L11-Panel.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../../Style/Panel.css"
+import Controls from '../L11-Controls/L11-Controls';
+
+const Level11Panel = () => {
+    return (
+        <div class="border">
+            <Controls/>
+        </div>
+    );
+}
+
+export default Level11Panel;
