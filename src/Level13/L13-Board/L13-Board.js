16cf0127d65c2c9b2b694b1b55dc884c6b768153
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2022-09-21 14:56:17 +1200

    Implemented level 13 and logic.

---
+++ src/Level13/L13-Board/L13-Board.js
@@ -0,0 +1,46 @@
+import React from "react"
+import "../../Style/Board.css"
+import Panel from "../L13-Panel/L13-Panel"
+
+const state = {
+    board: Array(9).fill(null),
+    player: null
+}
+
+const Level13Board = () => {
+
+    const renderBoxes = () => {
+        return state.board.map(
+            (box, index) =>
+                <div className="box" data-row={parseInt(index / 3)} data-col={index % 3}>
+                    {box}
+                </div>
+        )
+    }
+
+
+    state.board[8] = <img class="image" src="assets/dog.png" alt="d" id="dog"></img>
+
+    state.board[3] = <img class="bone" src="assets/food.png" alt="f" id="food"></img>
+
+    state.board[7] = <img class="hole" src="assets/hole.png" alt="h" id="holeOne"></img>
+
+    state.board[5] = <img class="hole" src="assets/hole.png" alt="h" id="holeTwo"></img>
+
+
+    return (
+        <div className="container" >
+            <div className="boardcontainer2">
+                <div className="board13" >
+                    {renderBoxes()}
+                </div>
+            </div>
+            <div className="bottom-panel">
+                <Panel />
+            </div>
+
+        </div>
+    )
+}
+
+export default Level13Board;
---
+++ src/Level13/L13-Panel/L13-Panel.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../../Style/Panel.css"
+import Controls from '../L13-Controls/L13-Controls';
+
+const Level13Panel = () => {
+    return (
+        <div class="border">
+            <Controls/>
+        </div>
+    );
+}
+
+export default Level13Panel;
