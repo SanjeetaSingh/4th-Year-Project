38300d93681cdabe2ffe7528ee7fd218760f0847
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2022-10-03 23:20:30 +1300

    Implement level 21.

---
+++ src/Level21/L21-Board/L21-Board.js
@@ -0,0 +1,64 @@
+import React from "react"
+import "../../Style/Board.css"
+import Panel from "../L21-Panel/L21-Panel"
+
+
+const state = {
+    board: Array(15).fill(null),
+}
+
+/**
+ * Class constucts the grid board with 
+ * the dog, paws, food and paw object on it.
+ * 
+ * @returns a grid board
+ */
+function Level21Board() {
+    const renderBoxes = () => {
+        return state.board.map(
+            (box, index) =>
+                <div className="box" data-row={parseInt(index / 5)} data-col={index % 5}>
+                    {box}
+
+                </div>
+        )
+    }
+
+
+    state.board[0] = <img class="ball" src="assets/ball.png" alt="d" id="ball"></img>;
+    state.board[1] = <img class="goal" src="assets/food.png" alt="d" id="foodTop1"></img>;
+    state.board[3] = <img class="goal" src="assets/bone.png" alt="d" id="foodTop"></img>;
+    state.board[10] = <img class="image" src="assets/dog.png" alt="d" id="dog"></img>;
+    state.board[2] = <img class="ball" src="assets/ball.png" alt="d" id="ball2"></img>;
+
+
+
+    return (
+        <div className="container" >
+            <div className="boardcontainer2">
+
+                <div class="numbers">
+                    ArrayList &nbsp;&nbsp;&nbsp;&nbsp;
+                    <br />
+                    <br />
+                    &nbsp;&nbsp;&nbsp;&nbsp; [ 0 
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    1 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    2 &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    3 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;
+                    4 ] &nbsp;&nbsp;
+                </div>
+                <div className="board16" >
+                    {renderBoxes()}
+                </div>
+            </div>
+            <div className="bottom-panel">
+                <Panel />
+            </div>
+        </div>
+    )
+}
+
+
+
+export default Level21Board
---
+++ src/Level21/L21-Panel/L21-Panel.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../../Style/Panel.css"
+import Controls from '../L21-Controls/L121-Controls';
+
+const Level21Panel = () => {
+    return (
+        <div class="border">
+            <Controls/>
+        </div>
+    );
+}
+
+export default Level21Panel;
