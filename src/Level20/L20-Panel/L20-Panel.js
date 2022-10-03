255bccb211ec49b6e9ebaabd210ca65f5086c25a
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2022-10-03 22:29:51 +1300

    Implemented level 20 with the logic.

---
+++ src/Level20/L20-Panel/L20-Panel.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../../Style/Panel.css"
+import Controls from '../L20-Controls/L120-Controls';
+
+const Level20Panel = () => {
+    return (
+        <div class="border">
+            <Controls/>
+        </div>
+    );
+}
+
+export default Level20Panel;
