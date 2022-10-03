1f621d5af7e79f84a34c7d2517b8eeac7bcc92cc
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2022-10-03 22:30:17 +1300

    Called on the level in the main function and fixed styling.

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ import Board16  from './Level16/L16-Board/L16-Board'
 import Board17 from './Level17/L17-Board/L17-Board'
 import Board18 from './Level18/L18-Board/L18-Board'
 import Board19 from './Level19/L19-Board/L19-Board'
+import Board20 from './Level20/L20-Board/L20-Board'
 import Header from './Header/Header';
 
 function App() {
@@ -53,6 +54,7 @@ function App() {
           <Route path='/level17'><Board17/></Route>
           <Route path='/level18'><Board18/></Route>
           <Route path='/level19'><Board19/></Route>
+          <Route path='/level20'><Board20/></Route>
         </Switch>
       </div>
     </Router>
